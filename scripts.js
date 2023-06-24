ca3ae6bb75e5da28823c7d55c7c442a9c62e736f
redwing45 <19aar94@gmail.com>
2023-06-23 20:03:34 -0500

    WorkingWithTAAshleyG

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
 function init () {
-const takeoff = document.getElementById("takeoff")
-const flightStatus = document.getElementById("flightStatus")
+let takeoff = document.getElementById("takeoff")
+let flightStatus = document.getElementById("flightStatus")
 let shuttleBackground = document.getElementById("shuttleBackground")
 let spaceShuttleHeight = document.getElementById("spaceShuttleHeight")
-const landing = document.getElementById("landing")
-const up = document.getElementById("up")
-const down = document.getElementById("down")
-const left = document.getElementById("left")
-const right = document.getElementById("right")
-const abortMission = document.getElementById("missionAbort")
+let landing = document.getElementById("landing")
+let up = document.getElementById("up")
+let down = document.getElementById("down")
+let left = document.getElementById("left")
+let right = document.getElementById("right")
+let abortMission = document.getElementById("missionAbort")
+let rocket = document.getElementById("rocket")
+rocket.style.position = "absolute"
+rocket.style.left = "0px"
+rocket.style.bottom = "0px"
+// let shuttleWidth = document.getElementById("shuttleWidth")
 
 takeoff.addEventListener("click", (event) =>{
     let response = window.confirm("Confirm that the shuttle is ready for takeoff")
         if(response === true){
             flightStatus.innerHTML = "Shuttle in flight"
-            shuttleBackground.style.backgroundColor === 'blue'
-            spaceShuttleHeight += 10000
+            shuttleBackground.style.backgroundColor = 'blue'
+            spaceShuttleHeight.innerHTML = '10000'
         }
 
    
@@ -24,35 +29,39 @@ takeoff.addEventListener("click", (event) =>{
 landing.addEventListener("click", (event) =>{
     window.alert("The shuttle is landing. Landing gear engaged.")
     flightStatus.innerHTML = "The shuttle has landed."
-    shuttleBackground.style.backgroundColor === " "
-    spaceShuttleHeight = 0
+    shuttleBackground.style.backgroundColor = 'green'
+    spaceShuttleHeight.innerHTML = '0';
+    rocket.style.bottom = "0px";
+    rocket.style.left = "0px";
 })
 
 abortMission.addEventListener("click", (event) =>{
     let response = window.confirm("Confirm that you want to abort the mission.")
         if(response = true){
             flightStatus.innerHTML =  "Mission aborted."
-            shuttleBackground.style.backgroundColor === ' '
-            spaceShuttleHeight = 0
+            shuttleBackground.style.backgroundColor = 'green'
+            spaceShuttleHeight.innerHTML = '0'
+            rocket.style.bottom = "0px";
+            rocket.style.left = "0px";
+
         }
 
 })
-left.addEventListener("click", (event) =>{
-        shuttleBackground.width = shuttleBackground.width - 10
-        spaceShuttleHeight = 0
-})
+    left.addEventListener("click", (event) =>{
+        rocket.style.left = '10px'
+    })
 
     right.addEventListener("click", (event) =>{
-        shuttleBackground.width = shuttleBackground.width + 10
-        spaceShuttleHeight = 0
+        rocket.style.right = '10px'
     })
+
     up.addEventListener("click", (event) =>{
-        shuttleBackground.height = shuttleBackground.height + 10
-        spaceShuttleHeight += 10
+        rocket.style.top = '10px'
+        spaceShuttleHeight.innerHTML = (spaceShuttleHeight.innerHTML) + 10000
     })
     down.addEventListener("click", (event) =>{
-        shuttleBackground.height = shuttleBackground.height - 10
-        spaceShuttleHeight -= 10
+        rocket.style.bottom = '10px'
+        spaceShuttleHeight.innerHTML = (spaceShuttleHeight.innerHTML) - 10000
     })
 }
 window.addEventListener("load", init);
